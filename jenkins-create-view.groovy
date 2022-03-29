b0e9a0206098a39adbe1d887953d53dafc2c550e
Gowthamv45 <102169334+Gowthamv45@users.noreply.github.com>
2022-03-29 19:00:55 +0530

    Update jenkins-create-view.groovy

--- jenkins-create-view.groovy
+++ jenkins-create-view.groovy
@@ -3,12 +3,14 @@
 // imports
 import jenkins.model.Jenkins
 import hudson.model.ListView
+//import hudson.views.BuildStatusFilter
+import hudson.views.StatusFilter
 
 // get Jenkins instance
 Jenkins jenkins = Jenkins.getInstance()
 
 // variables
-def viewName = 'MyView'
+def viewName = 'Programview5'
 
 // create the new view
 jenkins.addView(new ListView(viewName))
@@ -16,10 +18,15 @@ jenkins.addView(new ListView(viewName))
 // get the view
 myView = hudson.model.Hudson.instance.getView(viewName)
 
+//List<BuildStatusFilter> expectedFilters = new ArrayList<BuildStatusFilter>()
+//def filter = new BuildStatusFilter(true,false,false,"includeMatched")
+def filter = new StatusFilter(true)
+myView.getJobFilters().add(filter)
+
 // add a job by its name
-myView.doAddJobToView('MyJob1')
-myView.doAddJobToView('MyJob2')
-myView.doAddJobToView('MyJob3')
+//myView.doAddJobToView('MyJob1')
+//myView.doAddJobToView('MyJob2')
+//myView.doAddJobToView('MyJob3')
 
 // save current Jenkins state to disk
 jenkins.save()
